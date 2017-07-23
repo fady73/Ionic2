152116b2e3c772961eff1fc4b11235868e5e08b4
fady khayrat <fadykhayrat@gmail.com>
2017-07-23 10:29:30 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,8 +43,7 @@ export class MyApp {
      {
      this.showToastWithCloseButton();
      }
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
+
     this.nav.setRoot(page.component);
   }
 }
